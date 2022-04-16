781d4bfd1222441706aa1833975b5e1ff04a704c
Maxwell Chow <maxchowla@gmail.com>
2022-04-16 01:17:15 -0700

    change to 90 degree angles

--- yuki.py
+++ yuki.py
@@ -7,7 +7,8 @@ output = []
 with Image.open("snow.gif") as img:
 
 	# img.seek(0)
-	for theta in [0, -90, 180, 90]:
+	for x in range(4):
+		theta = -90 * x
 		img.seek(0)
 		try:
 			while 1:
@@ -20,4 +21,5 @@ with Image.open("snow.gif") as img:
 			pass
 
 #Save the first frame as a gif, with all subsequent frames appended. (just fucking saves it)
+#also set gif to loop and not maintain frame data
 output[0].save('out.gif', save_all=True, append_images=output[1:], loop=0, disposal=2)
